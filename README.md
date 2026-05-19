d46a4e71cfd6cf5437f0138be51369299d2f9b9a
Abhishek <abhishekkumarprabhatyt@gmail.com>
2026-05-19 19:07:27 +0530

    made every file meaning full

---
+++ README.md
@@ -0,0 +1,13 @@
+# Developer
+Abhishek Kumar Prabhat
+
+# Project
+User details form for learning how diffrent form elements tags works in html.
+
+# files
+form.html -> form structure. \n
+style.css -> heading font styles. \n
+app.js -> Register button listner.
+
+# Developer Contact
+ Msg me on [Telegram](tg.png)(https://t.me/Abhishek_returns).
--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello");
+let submitBtn = document.querySelector("#submitBtn");
+let fname = document.querySelector("#fname");
+let lname = document.querySelector("#lname");
+let category = document.querySelector('#gender');
+submitBtn.addEventListener("click", function (event) {
+event.preventDefault(); 
+console.log('your first name is ' , fname.value , ' , your last name is ' , lname.value, 'and your gender is ' , category .value );
+
+});
